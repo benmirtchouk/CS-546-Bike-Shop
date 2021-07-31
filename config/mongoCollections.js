4620df52c4f4f6bff3a38601a0e4b1f107fc4276
Ari Birnbaum <ari@ceiphr.com>
2021-07-31 18:18:04 -0400

    Added mongodb connection/config code.

---
+++ config/mongoCollections.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Provided from lecture notes.
+ ******************************************************************************/
+const dbConnection = require("./mongoConnection");
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return async () => {
+        if (!_col) {
+            const db = await dbConnection();
+            _col = await db.collection(collection);
+        }
+
+        return _col;
+    };
+};
+
+/* Now, you can list your collections here: */
+module.exports = {
+    users: getCollectionFn("users"),
+    products: getCollectionFn("products"),
+    reviews: getCollectionFn("reviews"),
+    orders: getCollectionFn("orders"),
+};
