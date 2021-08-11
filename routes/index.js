3b9649abd78e31c88947e1e752e0aec4c409fb65
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-08-10 20:56:28 -0400

    Implemented route logic for the landing page.
    Changes
    ---
    - Updated the formatting of the main handlebars
    - Created the landing page which shows up to 20 products
    - Basic styling to form data as cards with on hovers
    
    Testing
    ---
    - Tested on Chrome and FireFox views display correctly
    - HTML semantic checker to validate generated HTML was correct

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,33 @@
+const mongoCollections = require("../config/mongoCollections");
+const products = require("../data").products;
+
+
 const constructorMethod = (app) => {
+
+  app.use('/', async (req, res ) => {
+
+    const productList = await products.getAllUpTo(20);
+
+    // Map the model data to what handlebars can use
+    productList.map(product => {
+      product.isLowStock = product.stock < 5
+      product.price = product.price.toFixed(2);
+    });
+
+    const handlebarData = {
+      header: {
+        title: "Welcome to the Bike Shop!"
+      },
+      page: {
+        title: "Bike Shop"
+      },
+      bikes: productList
+      
+    }
+    res.render("layouts/homepage", handlebarData)
+  })
+
+
   app.use('*', (req, res) => {
     res.status(404).send();
   });
