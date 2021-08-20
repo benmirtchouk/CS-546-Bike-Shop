d6c652a16703b89f0a3c4abeca39f489a2b052a9
TerryDuan <terry.w.duan@gmail.com>
2021-08-20 01:48:20 -0400

    new func for orde
    new func on orders

--- data/index.js
+++ data/index.js
@@ -9,5 +9,18 @@ module.exports = {
   products: productsData,
   reviews: reviewsData,
   orders: ordersData,
+<<<<<<< HEAD
   metrics: metricsData
 };
+=======
+};
+
+async function main() {
+  //try removing the await keyword and run the application
+  console.log(await ordersData.getOrdersByUser('611f275a7aa262fdb0355bad'));
+
+}
+
+//comment this out if you uncomment main2 below
+main();
+>>>>>>> e90a01e (new func for orders)
