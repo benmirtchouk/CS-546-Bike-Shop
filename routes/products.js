f23e60e17b945e4366302c9e731d7e2f47139a88
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-08-22 22:05:56 -0400

    Admins can PUT new bikes
    Changes
    ---
    - PUT route exposed for new bikes for admins
    - Resolved the server never responding on get requests if the slug was not found.
    - Added placeholder image from lab 11

--- routes/products.js
+++ routes/products.js
@@ -6,9 +6,16 @@ const reviewData = require("../data/reviews");
 const metrics = require("../data/metrics");
 const productSchema = require("../schemas/new_product");
 const authHelper = require("../middleware/authentication");
+const slugify = require('slugify');
 
 router.get("/:slug", async (req, res) => {
+
+    // TODO handle null case
     const product = await productData.getBySlug(req.params.slug);
+    if(product == null) {
+        res.status(404).json({"message": "No product found for that name!"});
+        return;
+    }
     product.isOutOfStock = product.stock <= 0;
     product.isLowStock = product.stock < 5 && !product.isOutOfStock;
     metrics.notifyProductPageView(product._id);
@@ -18,6 +25,37 @@ router.get("/:slug", async (req, res) => {
 
 });
 
+router.put("/", async (req,res) => {
+    if (authHelper.getUserIfAdmin(req) == null) {
+        authHelper.endRequestDueToBeingUnauthorized(res);
+        return
+    }
+    if (!req.body.name || typeof req.body.name !== 'string') {
+        res.status(400).json({message: "Name must be specified"});
+        return;
+    }
+    req.body.slug = slugify(req.body.name, {lower: true})
+    req.body.pictures = ['/images/no_image.jpg']
+
+    const { error, value: validatedProduct } = productSchema.validate(req.body);
+    if (error) {
+        console.error(`Validation failed on put for ${req.body.name}! ${error}`);
+        let msg = `Validation error: ${error.details.map(x => x.message).join(', ')}`;
+        res.status(400).json({message: msg});
+        return
+    }
+
+    try {
+        await productData.create(validatedProduct);
+        res.status(204).json({});
+    } catch (e){
+        console.error(e);
+        const status = typeof e == 'string' && e.startsWith("Slug already in use") ? 400 : 500;
+        const message = status == 400 ? e : "Internal error applying update"
+        res.status(status).json({"message": message});
+    }
+
+})
 
 router.patch("/:id", async (req, res) => { 
     if (authHelper.getUserIfAdmin(req) == null) {
@@ -47,7 +85,7 @@ router.patch("/:id", async (req, res) => {
     if (error) {
         console.error(`Validation failed on patch for ${id}! ${error}`);
         let msg = `Validation error: ${error.details.map(x => x.message).join(', ')}`;
-        res.status(400).json({error: msg});
+        res.status(400).json({message: msg});
         return
     }
 
